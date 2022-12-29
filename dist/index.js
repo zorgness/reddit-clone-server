8b83542f9bf16f5b296508fda87254b10c04945e
Thomas Fouquert <tfouquert@gmail.com>
2022-12-29 20:01:05 +0100

    apollo server

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const postgresql_1 = require("@mikro-orm/postgresql");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
 const main = async () => {
     const orm = await postgresql_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
-    const emFork = orm.em.fork();
-    const posts = await emFork.find(Post_1.Post, {});
-    console.log(posts);
+    const app = (0, express_1.default)();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.HelloResolver],
+            validate: false,
+        }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log("server listening on port 4000");
+    });
 };
 main().catch((err) => console.error(err));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
 import { MikroORM } from "@mikro-orm/postgresql";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
 
-  const emFork = orm.em.fork();
-  // const post = await emFork.create(Post, {
-  //   title: "second post",
-  // } as Post);
-  // await emFork.persistAndFlush(post);
-  const posts = await emFork.find(Post, {});
-  console.log(posts);
+  const app = express();
+
+  // app.get("/", (_, res) => {
+  //   res.send("hello world!");
+  // });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server listening on port 4000");
+  });
+
+  // const emFork = orm.em.fork();
+  // // const post = await emFork.create(Post, {
+  // //   title: "second post",
+  // // } as Post);
+  // // await emFork.persistAndFlush(post);
+  // // const posts = await emFork.find(Post, {});
+  // // console.log(posts);
 };
 
 main().catch((err) => console.error(err));
